d146e69d527e9d6bc2ca9133177cf80ec56d5584
grant8827@yahoo.com <77510135+grant8827@users.noreply.github.com>
2023-02-07 20:04:42 -0500

    js css html added to home page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+//screen refresh
+$(document).ready(function() {
+    window.addEventListener('resize', function() {
+        "use strict";
+        window.location.reload();
+    });
+})
+
+//navbar toggle
 const menu = document.getElementById('barMenu');
 const link = document.getElementById('nav');
 const x = document.getElementById('x-bar')
@@ -21,11 +30,67 @@ x.addEventListener("click", () => {
     } else {
         menu.style.display = "block";
     }
+    if (x.style.display = "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
 });
 
 
 
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
+
+var owl = $('.bonner-img');
+owl.owlCarousel({
+    margin: 10,
+    loop: true,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+
+// togglerClick function
+function togglerClick() {
+    navToggler.classList.toggle('toggler-open');
+    navMenu.classList.toggle('open');
+}
+
+// navLinkClick function
+function navLinkClick() {
+    if (navMenu.classList.contains('open')) {
+        navToggler.click();
+    }
+}
 
 
 
