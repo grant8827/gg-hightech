fde05293d9c07c98a2fd0ba88bfba977dbe45fc0
grant8827 <grant8827@yahoo.com>
2023-02-16 21:11:15 -0500

    add to cart js css html was added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 //screen refresh
-$(document).ready(function() {
+/*$(document).ready(function() {
     window.addEventListener('resize', function() {
         "use strict";
         window.location.reload();
     });
-})
+})*/
 
 //navbar toggle
 const menu = document.getElementById('barMenu');
@@ -37,9 +37,98 @@ x.addEventListener("click", () => {
     }
 });
 
+//cart start
+$(function () {
+
+    $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
+
+    $(".item ").on("click", function () {
+       
+        $(this).addClass('disabled')  
+            //add items to basket
+            $(this).each(function () {
+               let name = $(this).children(".item-detail").children("h4").text();
+               let remove = "<button class='remove'> x </button>";
+                let cena = "<span class='eachPrice'>" + (parseFloat($(this).children(".item-detail").children(".prices").children(".price").text())) + "</span>";
+                $("#list-item").append("<li>" + name + "&#09; - &#09;" + "$" + cena + remove + "</li>");
+
+                //number of items in basket
+                $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
+                $("#items-basket").text();
+
+                //calculate total price
+                var totalPrice = 0;
+                $(".eachPrice").each(function () {
+                    var cenaEach = parseFloat($(this).text());
+                    totalPrice += cenaEach;
+                });
+                $("#total-price").text( "$" +totalPrice );
+            });
+
+        //remove items from basket
+        $(".remove").on("click", function () {
+            $(this).parent().remove();
+            var totalPrice = 0;
+            $(".eachPrice").each(function () {
+                var cenaEach = parseFloat($(this).text());
+                totalPrice += cenaEach;
+            });
+            $("#total-price").text(totalPrice + "$");
+            $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
+        });
+    });
+
+   //clear cart 
+});
+$(document).ready(function(){
+    $(".clear-cart").on("click", function () {
+        $("#list-item").empty();
+       
+    });
+    $(".clear-cart").on("click", function () {
+        $("#total-price").empty();
+       
+    });
+    $(".clear-cart").on("click", function () {
+        $("#items-basket").empty();
+       
+    });
+});
+
+// cart popup
+$(document).ready(function(){
+    $(".cart-toggle").click(function(){
+        $(".in-cart").fadeIn(500);
+    })
+    $(".close").click(function(){
+        $(".in-cart").fadeOut(500);
+    });
+});
 
 
+//cart pages
+$(document).ready(function(){
 
+    $('.addToCart').click(function (){
+        itemCount ++;
+        $('#itemCart').html(itemCount).css('display', 'block');
+
+$(".addToCart").click(function(){
+    $("ol").append("<li></li>")
+})
+       
+      }); 
+      
+      $('.clear').click(function() {
+        itemCount = 0;
+        $('#itemCount').html('').css('display', 'none');
+        $('#cartItems').html('');
+      }); 
+});
+var itemCount = 0;
+//cart end
+
+// slider in header start
 let slideIndex = 0;
 showSlides();
 
@@ -76,7 +165,48 @@ owl.owlCarousel({
             items: 3
         }
     }
-})
+});
+// head slider end
+
+//border carousel start
+(async ()=>   // async IIFE code for slider.
+  {
+  const
+    interval       = 1500  // ms
+  , paddingRight   = 0
+  , slideContainer = document.querySelector('.carousel') 
+  , slidesWrapper  = document.querySelector('.carousel-slides')
+  , slides         = document.querySelectorAll('.carousel-slides > li')
+  , delay          = ms => new Promise(r => setTimeout(r, ms))
+  , movLeft = (el, mov) => new Promise(r =>
+    {
+    el.ontransitionend =_=>
+      {
+      el.ontransitionend = null
+      el.style.transition = 'none';
+      r()
+      }
+    el.style.transition = '1s';
+    el.style.transform  = `translateX(${-mov}px)`;
+    });
+
+  let index = 0;
+
+  while (true) // infinite carrousel loop
+    {
+    await delay( interval )
+    await movLeft( slidesWrapper, slides[index].clientWidth + paddingRight  )
+
+    slidesWrapper.appendChild( slides[index] )  // mov first slide to the end
+    slidesWrapper.style.transform    = `translateX(0)` // rest translateX
+    index = ++index % slides.length
+    }
+  })()
+//border carousel end
+
+
+
+
 
 
 // togglerClick function
@@ -92,16 +222,26 @@ function navLinkClick() {
     }
 }
 
+$(document).ready(function(){ 
+   $(window).scroll(function(){ 
+       if ($(this).scrollTop() > 300) { 
+           $('#toTop').fadeIn(); 
+       } else { 
+           $('#toTop').fadeOut(); 
+       } 
+   }); 
+   $('#toTop').click(function(){ 
+       $("html, body").animate({ scrollTop: 0 }, 600); 
+       return false; 
+   }); 
+});
 
 
 
 
 
 
-
-
-
-let btn = document.getElementById("toTop");
+/*let btn = document.getElementById("toTop");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
@@ -125,3 +265,27 @@ function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+*/
+
+//about page
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
